2f89773432d1b82ef2ac93ee596b8a3b24b3786f
ajjo <ajjogames@gmail.com>
2020-02-21 12:22:12 -0800

    Leaderboard ui

---
+++ Assets/New/Scripts/LeaderboardUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardUI : MonoBehaviour
+{
+    public Transform cloneItem;
+    public Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0;i<15;i++)
+        {
+            Transform obj = Instantiate(cloneItem, parent);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
